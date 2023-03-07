d6e61de41555d41aee913d6e2605d4b2512f2bb6
Emre Karayaman <107217989+karayamanemre@users.noreply.github.com>
2023-03-07 10:34:46 +0300

    Update Register.js

--- src/Components/Pages/Register.js
+++ src/Components/Pages/Register.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-function Login() {
+const Login = () => {
   const [name, setName] = useState('');
   const [alert, setAlert] = useState('');
   const navigate = useNavigate();
@@ -85,5 +85,5 @@ function Login() {
       {alert && <span className="text-red-600 mt-2">{alert}</span>}
     </div>
   );
-}
+};
 export default Login;
